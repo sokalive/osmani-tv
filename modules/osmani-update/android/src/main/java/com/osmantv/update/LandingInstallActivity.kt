827324ee83b17abba575ba6114e157bfdf9d1484
sokalive <waziriissa37@gmail.com>
2026-07-06 15:56:59 -0700

    Revert "fix(bridge): add lifecycle dependency for LandingInstallActivity Gradle build."
    This reverts commit eeb103b5377c6e2d2f0214e7a0a792320829640c.

--- modules/osmani-update/android/src/main/java/com/osmantv/update/LandingInstallActivity.kt
+++ modules/osmani-update/android/src/main/java/com/osmantv/update/LandingInstallActivity.kt
@@ -5,7 +5,6 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.osmantv.update.R
 import kotlinx.coroutines.launch
 
 /**
